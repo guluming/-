622c9c70d177b8ae3aa4e5a0bad30dcfb9166f22
guluming <guluming@naver.com>
2022-09-29 20:59:10 +0900

    이진 변환 반복하기 풀이 끝

---
+++ src/난이도2/이진변환반복하기/Solution.java
@@ -0,0 +1,26 @@
+package 난이도2.이진변환반복하기;
+
+import java.util.*;
+
+public class Solution {
+    public static int[] solution(String s) {
+        int zero_count = 0;
+        int change_count = 0;
+        while (!s.equals("1")) {
+            zero_count = zero_count + s.length() - s.replace("0","").length();
+            s = Integer.toBinaryString(s.replace("0","").length());
+            change_count++;
+        }
+        return new int[]{change_count, zero_count};
+    }
+
+    public static void main(String[] args) {
+        String s = "110010101001";
+        int[] result = {3,8};
+        if (Arrays.equals(solution(s), result)) {
+            System.out.println("통과했습니다.");
+        } else {
+            System.out.println("불일치했습니다.");
+        }
+    }
+}
