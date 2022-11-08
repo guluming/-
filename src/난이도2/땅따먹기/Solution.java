0cb9a568a30f0ead433157b99d3a559a312494ee
guluming <guluming@naver.com>
2022-11-08 23:18:44 +0900

    땅따먹기 풀이 끝

---
+++ src/난이도2/땅따먹기/Solution.java
@@ -0,0 +1,31 @@
+package 난이도2.땅따먹기;
+
+import java.util.*;
+
+public class Solution {
+    private static int solution(int[][] land) {
+        int answer = 0;
+        for (int i = 1; i < land.length; i++) {
+            land[i][0] += Math.max(land[i - 1][1], Math.max(land[i - 1][2], land[i - 1][3]));
+            land[i][1] += Math.max(land[i - 1][0], Math.max(land[i - 1][2], land[i - 1][3]));
+            land[i][2] += Math.max(land[i - 1][0], Math.max(land[i - 1][1], land[i - 1][3]));
+            land[i][3] += Math.max(land[i - 1][0], Math.max(land[i - 1][1], land[i - 1][2]));
+        }
+
+        for (int i = 0; i < 4; i++) {
+            answer = Math.max(answer, land[land.length - 1][i]);
+        }
+
+        return answer;
+    }
+
+    public static void main(String[] args) {
+        int[][] land = {{1, 2, 3, 5}, {5, 6, 7, 8}, {4, 3, 2, 1}};
+        int result = 16;
+        if (solution(land) == result) {
+            System.out.println("통과했습니다.");
+        } else {
+            System.out.println("불일치했습니다.");
+        }
+    }
+}
